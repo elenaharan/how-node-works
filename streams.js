19bcc95ab59daf43f0f3ba5106cd5e48f16f0745
Elena <elenaharan@gmail.com>
2024-11-03 08:35:20 +0000

    use pipe method to avoid backpressure

--- streams.js
+++ streams.js
@@ -12,27 +12,29 @@ server.on("request", (req, res) => {
   //   if (err) console.log(err);
   //   res.end(data);
   // });
-
   //**************************SOLUTION 2: STREAMS ****************************************/
   //In this solution we don't need to save the data into a variable and then send it back to the client
   //We can send the data to the server as a stream in chunks, using readable streams
   //and then send it to the client using writable streams
-  const readable = fs.createReadStream("test-file.txt");
+  // const readable = fs.createReadStream("test-file.txt");
   //each time there is a data available to consume, it will emit "data" event:
-  readable.on("data", (chunk) => {
-    //this sends the available chunk of data to the client in a response
-    res.write(chunk);
-  });
-
+  // readable.on("data", (chunk) => {
+  //this sends the available chunk of data to the client in a response
+  //   res.write(chunk);
+  // });
   //we also need to listen for an event when all data is read
   //without the res.end the solution wouldn't work because the response would never be sent to the client
-  readable.on("end", () => res.end());
-
-  readable.on("error", (err) => {
-    console.log(err);
-    res.statusCode = 500;
-    res.end("File not found!");
-  });
+  // readable.on("end", () => res.end());
+  // readable.on("error", (err) => {
+  //   console.log(err);
+  //   res.statusCode = 500;
+  //   res.end("File not found!");
+  // });
+  //***************************SOLUTION 3: handling backpressure - best solution*/
+  //backpressure occurs when the response cannot be sent as quickly as requests are received
+  const readable = fs.createReadStream("test-file.txt");
+  //readableStream.pipe(writableStream) - writable stream can also be a duplex or transform
+  readable.pipe(res);
 });
 
 server.listen(8000, "127.0.0.1", () => {
