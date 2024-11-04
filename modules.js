d44931842212e685c9ddab9b024b5f2531cfc8e7
Elena <elenaharan@gmail.com>
2024-11-04 06:26:45 +0000

    export/import multiple functions

--- modules.js
+++ modules.js
@@ -6,3 +6,10 @@ const calc1 = new C();
 console.log(calc1.add(2, 5));
 
 //exports
+const { add, multiply, divide } = require("./test-module-2");
+console.log(multiply(2, 5));
+
+//caching
+// require("./test-module-3")();
+// require("./test-module-3")();
+// require("./test-module-3")();
